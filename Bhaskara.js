1cca1bc0c1239bad0315522785fddc625d57a624
Erikles Paixão <153569600+EriklesJounetsu@users.noreply.github.com>
2024-05-24 16:09:33 -0300

    Update Bhaskara.js
    - havia um erro de PEMDAS, em que  a divisão será feita com o 2 antes que ele possa multiplicar-se com o subsequente
    -reformular aquela função de potencializar
    trocar o x*x por “ Math.pow(x,2)”

--- Bhaskara.js
+++ Bhaskara.js
@@ -8,8 +8,8 @@ function Bhaskara(a,b,c){
 var a = ler_entrada("a");
 var b = ler_entrada("b");
 var c = ler_entrada("c");
-var x1 = (-b + raiz_quadrada(delta(a,b,c)))/2*a; 
-var x2 = (-b - raiz_quadrada(delta(a,b,c)))/2*a;
+var x1 = (-b + raiz_quadrada(delta(a,b,c)))/(2*a); 
+var x2 = (-b - raiz_quadrada(delta(a,b,c)))/(2*a);
 escrever_saida("saida", "x¹ = "+ x1 + "<br>" + "x² = " + x2);
 }
 function ler_entrada(campo){
@@ -22,7 +22,7 @@ function raiz_quadrada(x) {
   return Math.sqrt(x);
 }
 function quadrado(x){
-return x*x;
+return Math.pow(x,2);
 }
 function delta(a,b,c){
   return quadrado(b)+((-4*a)*c); 
